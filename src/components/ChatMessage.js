1c652aaced5be0d8db62056bb9985b5bdb2562f2
KarimTaoui <130943395+KarimTaoui@users.noreply.github.com>
2024-05-02 21:11:10 +0100

    Update ChatMessage.js

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import remarkGfm from 'remark-gfm';
 import moment from 'moment';
 import person from '../assets/person.png';
