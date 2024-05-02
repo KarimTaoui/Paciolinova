a564487cfc219a9b4d21d6bc6c9699788fd012f1
KarimTaoui <130943395+KarimTaoui@users.noreply.github.com>
2024-05-02 20:57:46 +0100

    Update ChatView.js

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -1,18 +1,41 @@
 import React, { useState, useRef, useEffect, useContext } from 'react';
 import ChatMessage from './ChatMessage';
 import { ChatContext } from '../context/chatContext';
-import { MdSend } from 'react-icons/md';
+import { MdSend, MdLightbulbOutline } from 'react-icons/md';
+import 'react-tooltip/dist/react-tooltip.css';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import Modal from './Modal';
+import Setting from './Setting';
+import PromptPerfect from './PromptPerfect';
+import moment from 'moment';
+import 'moment/locale/fr'; // Importez le module de localisation français
 
+/**
+ * A chat view component that displays a list of messages and a form for sending new messages.
+ */
 const ChatView = () => {
   const messagesEndRef = useRef();
   const inputRef = useRef();
   const [formValue, setFormValue] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [loading, setLoading] = useState(false);
   const [messages, addMessage] = useContext(ChatContext);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalPromptOpen, setModalPromptOpen] = useState(false);
 
+  /**
+   * Scrolls the chat area to the bottom.
+   */
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
+  /**
+   * Adds a new message to the chat.
+   *
+   * @param {string} newValue - The text of the new message.
+   * @param {boolean} [ai=false] - Whether the message was sent by an AI or the user.
+   */
   const updateMessage = (newValue, ai = false) => {
     const id = Date.now() + Math.floor(Math.random() * 1000000);
     const newMsg = {
@@ -25,6 +48,11 @@ const ChatView = () => {
     addMessage(newMsg);
   };
 
+  /**
+   * Sends our prompt to our API and get response to our request from openai.
+   *
+   * @param {Event} e - The submit event of the form.
+   */
   const sendMessage = async (e) => {
     e.preventDefault();
     if (!formValue) return;
@@ -62,6 +90,7 @@ const ChatView = () => {
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter' && !e.shiftKey) {
+      // 👇 Get input value
       sendMessage(e);
       inputRef.current.style.height = 'auto';
     }
@@ -71,10 +100,54 @@ const ChatView = () => {
     setFormValue(event.target.value);
   };
 
+  const updatePrompt = async () => {
+    const api = 'https://us-central1-prompt-ops.cloudfunctions.net/optimize';
+    const secretKey = process.env.REACT_APP_API_KEY;
+
+    try {
+      setLoading(true);
+      const response = await fetch(api, {
+        headers: {
+          'x-api-key': `token ${secretKey}`,
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          data: {
+            prompt: formValue.trim(),
+            targetModel: 'chatgpt',
+          },
+        }),
+        method: 'POST',
+      });
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+
+      const responseData = await response.json();
+      setPrompt(responseData.result.promptOptimized);
+      setLoading(false);
+      setModalPromptOpen(true);
+    } catch (e) {
+      console.error(e);
+      setLoading(false);
+    }
+  };
+
+  const handleUseClicked = () => {
+    setFormValue(prompt);
+    setModalPromptOpen(false);
+  };
+
+  /**
+   * Scrolls the chat area to the bottom when the messages array is updated.
+   */
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
 
+  /**
+   * Focuses the TextArea input to when the component is first rendered.
+   */
   useEffect(() => {
     inputRef.current.focus();
   }, []);
@@ -83,6 +156,7 @@ const ChatView = () => {
     inputRef.current.style.height = 'auto';
     inputRef.current.style.height = inputRef.current.scrollHeight + 'px';
   }, [formValue]);
+
 
   return (
     <div className="chatview">
@@ -106,9 +180,46 @@ const ChatView = () => {
             <button type="submit" className="chatview__btn-send" disabled={!formValue}>
               <MdSend size={30} />
             </button>
+            <div
+              className="loading-spinner"
+              style={{ marginLeft: '10px', display: loading ? 'flex' : 'none', alignItems: 'center', position: 'relative' }}
+            >
+              <div className="dot1"></div>
+              <div className="dot2"></div>
+              <div className="dot3"></div>
+              <span className="typing-text" style={{ marginLeft: '10px', color: '#4CAF50', fontSize: '14px' }}>
+                Chatbot is typing...
+              </span>
+            </div>
+            <button
+              id="tooltip"
+              type="button"
+              className="chatview__btn-send"
+              disabled={!formValue}
+              onClick={updatePrompt}
+            >
+              <MdLightbulbOutline size={30} />
+            </button>
           </div>
         </div>
+        <ReactTooltip
+          anchorId="tooltip"
+          place="top"
+          variant="dark"
+          content="Help me with this prompt!"
+        />
       </form>
+      <Modal title="Setting" modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <Setting modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      </Modal>
+      <Modal title="Prompt Perfect" modalOpen={modalPromptOpen} setModalOpen={setModalPromptOpen}>
+        <PromptPerfect
+          prompt={prompt}
+          onChange={setPrompt}
+          onCancelClicked={() => setModalPromptOpen(false)}
+          onUseClicked={handleUseClicked}
+        />
+      </Modal>
     </div>
   );
 };
