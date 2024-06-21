892d0b21ad14b72d576e8c7a2a7812361de69982
KarimTaoui <130943395+KarimTaoui@users.noreply.github.com>
2024-06-21 01:28:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ChatContextProvider } from './context/chatContext';
 import SideBar from './components/SideBar';
 import ChatView from './components/ChatView';
 import icon from './assets/Logo.ico'; // Import the icon file
+import Login from './Login'; // Import the Login component
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     document.title = 'Paciolinova'; // Change the title of the window
     const link = document.querySelector("link[rel~='icon']") || document.createElement('link'); // Find existing icon link element or create a new one
@@ -13,6 +16,14 @@ const App = () => {
     link.href = icon; // Set the icon file path
     document.getElementsByTagName('head')[0].appendChild(link);
   }, []);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  if (!isLoggedIn) {
+    return <Login onLogin={handleLogin} />;
+  }
 
   return (
     <ChatContextProvider>
